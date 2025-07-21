da686e60eb6395fb73e2fa38015057c797442928
Dzmitry Orgish <ljte823@gmail.com>
2025-07-21 14:29:30 +0300

    prefix expressions

--- eval/eval.go
+++ eval/eval.go
@@ -5,16 +5,24 @@ import (
 	"monkey/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.PrefixExpression:
+		return evalPrefixExp(node)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return toBoolean(node.Value)
 	default:
 		return nil
 	}
@@ -26,4 +34,45 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(stmt)
 	}
 	return result
+}
+
+func toBoolean(val bool) *object.Boolean {
+	if val {
+		return TRUE
+	}
+	return FALSE
+}
+
+func evalPrefixExp(node *ast.PrefixExpression) object.Object {
+	val := Eval(node.Right)
+
+	switch node.Operator {
+	case "!":
+		return evalBang(val)
+	case "-":
+		return evalMinus(val)
+	default:
+		return NULL
+	}
+}
+
+func evalBang(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinus(right object.Object) object.Object {
+	if right.Type() != object.OBJ_INTEGER {
+		return NULL
+	}
+	integer := right.(*object.Integer)
+	return &object.Integer{Value: -integer.Value}
 }
